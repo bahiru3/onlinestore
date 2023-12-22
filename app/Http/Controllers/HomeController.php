d4bfa405e96d6dd86dda4f69c777e9edfa7f711e
bahiru3 <bahirumegersa681@gmail.com>
2023-12-22 10:51:49 +0300

    test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,30 +6,71 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     //
-    public function about()
-    {
-        // return "well come to store";
-        //return "w
-       $title="About us Online Store";
-       $subtitle="About us Page";
-       $description="this about page -----";
-       $author="Developed By fname and lastname";
-       return view('home.about')-> with('title',$title)
-       ->with('subtitle',$subtitle)
-       ->with('description',$description)
-       ->with('author',$author);
-    }
-    public function index()
-    {
+    // public function about()
+    // {
+    //     // return "well come to store";
+    //     //return "w
+    //    $title="About us Online Store";
+    //    $subtitle="About us Page";
+    //    $description="this about page -----";
+    //    $author="Developed By fname and lastname";
+    //    return view('home.about')-> with('title',$title)
+    //    ->with('subtitle',$subtitle)
+    //    ->with('description',$description)
+    //    ->with('author',$author);
+    // }
+    // public function index()
+    // {
        
-       $title="About us Online Store";
-       $subtitle="Major Chatagories In Our Shope";
-       $imageUrls=['https://placehold.co/350',
-       'https://placehold.co/350',
-       'https://placehold.co/350'];
-             return view('home.index')->with('title',$title)
-       ->with('subtitle',$subtitle)
-       -> with('imageUrls',$imageUrls);
-    }
+    //    $title="About us Online Store";
+    //    $subtitle="Major Chatagories In Our Shope";
+    //    $imageUrls=['https://placehold.co/350',
+    //    'https://placehold.co/350',
+    //    'https://placehold.co/350'];
+    //          return view('home.index')->with('title',$title)
+    //    ->with('subtitle',$subtitle)
+    //    -> with('imageUrls',$imageUrls);
+    // }
+    // public function index()
+    // {
+       
+    //    return view("home.index");
+    // }
+//     public function index()
+
+
+//    {
+
+
+//        $viewData = [];
+
+
+//        $viewData["title"] = "Home Page - Online Store";
+
+
+//        return view('home.index')->with("viewData", $viewData);
+//    }
+public function index()
+{
+$viewData = [];
+$viewData["title"] = "Home Page - Online Store";
+return view('home.index')->with("viewData", $viewData);
+}
+
+
+//    
+public function about()
+{
+$data1 = "About us - Online Store";
+$data2 = "About us";
+$description = "This is an about page ...";
+$author = "Developed by: Your Name";
+return view('home.about')->with("title", $data1)
+->with("subtitle", $data2)
+->with("description", $description)
+->with("author", $author);
+}
+
+
 
 }
--- routes/web.php
+++ routes/web.php
@@ -12,37 +12,17 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/home','\App\Http\Controllers\HomeController@index');
 Route::get('/pages/employee-list-for','\App\Http\Controllers\PagesController@employee_list_for');
 Route::get('/pages/contact2/{display}','\App\Http\Controllers\PagesController@contact2');
 Route::get('/pages/contact1','\App\Http\Controllers\PagesController@contact1');
-Route::get('/','\App\Http\Controllers\HomeController@index')->name("home.index");
+Route::get('/index','\App\Http\Controllers\HomeController@index')->name("home.index");
 Route::get('about','\App\Http\Controllers\HomeController@about')->name("home.about");
 Route::get('/pages/employee-about','\App\Http\Controllers\PagesController@employee_about');
 Route::get('/pages/employee_list','\App\Http\Controllers\PagesController@employee_list');
 Route::get('/pages/employees','\App\Http\Controllers\PagesController@employees');
 Route::get('/pages/display/{view}','\App\Http\Controllers\PagesController@display');
-// Route::resource('/post','App\Http\Controllers\PostController',
-// Route::resource('/Test','\App\Http\Controllers\TestController',['only'=>['index']]);
-//only show rout create and show
-// Route::resource('/post','App\Http\Controllers\PostController',
-// [
-// 'only'=>['create','show']
-// ]);
-// Route::resource('/Student','App\Http\Controllers\PostController',
-// [
-// 'name'=>['create'=>'Student.bulid']
-// ]);
-//only show rout create and show
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-// Route::get('/', function () {
-//     return view('index');
-// });
-// Route::get('/about', function () {
-//     return view('about');
-// });
 
 
 
