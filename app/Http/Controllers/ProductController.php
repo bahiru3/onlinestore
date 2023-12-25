800db058bc902989434a7365ca0a87ffbd4b5df3
bahiru3 <bahirumegersa681@gmail.com>
2023-12-25 09:31:01 +0300

    onlinestore

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    
    public static $products = [
        ["id" => "1", "name" => "TV", "description" => "Best TV", "image" => "game.png", "price" => "1000"],
        ["id" => "2", "name" => "iPhone", "description" => "Best iPhone", "image" => "safe.png", "price" => "999"],
@@ -15,16 +15,28 @@ class ProductController extends Controller
    ];
    public function index()
    {
-       $viewData = [];
+    //    
+    // $viewData = [];
+    //    $viewData["title"] = "Products - Online Store";
+    //    $viewData["subtitle"] = "List of products";
+    //    $viewData["products"] = ProductController::$products;
+    //    return view('product.index')->with("viewData", $viewData);
+    $viewData = [];
        $viewData["title"] = "Products - Online Store";
        $viewData["subtitle"] = "List of products";
-       $viewData["products"] = ProductController::$products;
+       $viewData["products"] = Product::all();
        return view('product.index')->with("viewData", $viewData);
    }
    public function show($id)
    {
-       $viewData = [];
-       $product = ProductController::$products[$id - 1];
+    //    $viewData = [];
+    //    $product = ProductController::$products[$id - 1];
+    //    $viewData["title"] = $product["name"] . " - Online Store";
+    //    $viewData["subtitle"] = $product["name"] . " - Product information";
+    //    $viewData["product"] = $product;
+    //    return view('product.show')->with("viewData", $viewData);
+    $viewData = [];
+       $product = Product::findOrFail($id);
        $viewData["title"] = $product["name"] . " - Online Store";
        $viewData["subtitle"] = $product["name"] . " - Product information";
        $viewData["product"] = $product;
